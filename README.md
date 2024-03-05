0d0e17a786d34f75ea0e59ba2c29b48c51182a73
saurav-sanu <sauravsanu100@gmail.com>
2024-03-05 14:29:43 +0530

    razorpay clone

---
+++ README.md
@@ -0,0 +1,1 @@
+# RAZORPAY-CLONE
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontfamily:
+      ['Be Vietnam Pro','sans-serif'],
+      fontfamily:{
+        mullish:['Mulish', 'sans-serif'],
+    },
+    colors:{
+      deepblue:"#02042a",
+      lightblue:"#2b84ea",
+      lightblue300:"#4b94ed",
+      lightblue500:"#0b72e7",
+      greenlight:"#61cea6",
+      graytext:"#818597",
+      lightgray:"#e2e2e2",
+      grayblue:"#344a6c",
+      deepbluehead:"#162f56",
+      gray2:"#525a76",
+    },},
+  },
+  plugins: [],
+}
+
